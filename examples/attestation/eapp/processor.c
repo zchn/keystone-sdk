d3561d5d3bb47545f4181e2146685e48e3e06f0a
kevin <kevin@penguin>
2021-07-10 13:04:34 +0800

    Add logic for attesting enclave and copying out report in processor.c.

--- examples/attestation/eapp/processor.c
+++ examples/attestation/eapp/processor.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 
+#include "app/syscall.h"
+
+void ocall_copy_report(void* report, size_t len) {
+    ocall(3, report, len, 0, 0);
+}
+
 int main()
 {
   printf("I am the processor in the enclave!\n");
+
+  char* data = "nonce";
+  char buffer[2048];
+
+  attest_enclave((void*) buffer, data, 5);
+
+  ocall_copy_report(buffer, 2048);
+
   return 0;
 }
